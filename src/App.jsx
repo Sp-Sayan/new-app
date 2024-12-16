3f913186e7487e179f8b86132e4b7947cdf484a8
Sayan Paul <131338948+Sp-Sayan@users.noreply.github.com>
2024-12-16 21:43:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-full w-full bg-slate-500 flex items-center justify-center">
+      <p className="font-light text-5xl text-slate-200">
+        Welcome to Vite + React + Tailwind !!!
+      </p>
+    </div>
+  );
+}
+
+export default App;
