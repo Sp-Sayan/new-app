0e68ff23d826348770c96e55770c2fb5fd5f163b
Sayan Paul <131338948+Sp-Sayan@users.noreply.github.com>
2025-02-08 16:10:47 +0530

    added toggle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./components/ui/button";
-
-
+import Homepage from "./pages/Homepage.jsx";
+import { Switch } from "./components/ui/switch";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
     // <div className="h-full w-full bg-slate-500 flex items-center justify-center">
     //   <p className="font-light text-5xl text-slate-200">
     //      Welcome to Vite + React + Tailwind !!!
     //   </p>
     // </div>
-    <div className="flex items-center justify-center bg-background h-full w-full">
-      <Button className="relative text-foreground w-32 h-12">Click</Button>
+    <div className="flex items-center justify-evenly bg-background h-full w-full">
+      <Homepage />
+      <Switch onCheckedChange={toggleTheme}></Switch>
     </div>
   );
 }
